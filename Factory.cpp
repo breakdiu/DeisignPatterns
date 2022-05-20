8df37d8b1724d01d6516ed1a54f186f411b3583e
mike <2546771819@qq.com>
2022-05-20 15:09:07 +0800

    'recommit'

--- Factory.cpp
+++ Factory.cpp
@@ -1,20 +1,20 @@
 /**
-* The porduct interface  declares the operations 
+* The porduct interface  declares the operations
 *that all concerte products must implement.
-* 
+*
 */
 #include <iostream>
 
 /*
-* 
+*
  工厂方法模式优缺点
  你可以避免创建者和具体产品之间的紧密耦合。
  单一职责原则。 你可以将产品创建代码放在程序的单一位置， 从而使得代码更容易维护。
  开闭原则。 无需更改现有客户端代码， 你就可以在程序中引入新的产品类型。
 
- 
+
  应用工厂方法模式需要引入许多新的子类， 代码可能会因此变得更复杂。 最好的情况是将该模式引入创建者类的现有层次结构中。
- 
+
 */
 
 class Product {
@@ -121,7 +121,7 @@ void ClientCode(const Creator& creator) {
 }
 
 /**
-* 
+*
 * The app picks a creator's type depending on the configuration or environment.
 */
 
